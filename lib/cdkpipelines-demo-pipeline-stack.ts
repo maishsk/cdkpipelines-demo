ca95e73d13debc5099efed0c1afbda81872de66a
Maish Saidel-Keesing <maishsk@amazon.com>
2021-04-26 23:37:26 +0300

    error in unit test

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -54,7 +54,7 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
       },
       commands: [
         // Use 'curl' to GET the given URL and fail if it returns an error
-        'curl -Ssf $ENDPOINT_URL-baderror',
+        'curl -Ssf baderror-$ENDPOINT_URL',
       ],
     }));    
     pipeline.addApplicationStage(new CdkpipelinesDemoStage(this, 'QA-test', {
