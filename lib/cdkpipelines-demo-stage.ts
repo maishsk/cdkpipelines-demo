d8319b00889393e24c8d2792c8c7788cef66865a
Maish Saidel-Keesing <maishsk@amazon.com>
2021-04-26 22:13:10 +0300

    updated commmit

--- lib/cdkpipelines-demo-stage.ts
+++ lib/cdkpipelines-demo-stage.ts
@@ -1,5 +1,3 @@
-
-
 import { CfnOutput, Construct, Stage, StageProps } from '@aws-cdk/core';
 import { CdkpipelinesDemoStack } from './cdkpipelines-demo-stack';
 
@@ -16,51 +14,5 @@ export class CdkpipelinesDemoStage extends Stage {
     
     // Expose CdkpipelinesDemoStack's output one level higher
     this.urlOutput = service.urlOutput;
-  }
-}
-
-To organize things neatly, put the pipeline definition into its own stack file, lib/cdkpipelines-demo-pipeline-stack.ts (remember to replace OWNER and REPO in the code below):
-
-import * as codepipeline from '@aws-cdk/aws-codepipeline';
-import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
-import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
-import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
-
-/**
- * The stack that defines the application pipeline
- */
-export class CdkpipelinesDemoPipelineStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
-
-    const sourceArtifact = new codepipeline.Artifact();
-    const cloudAssemblyArtifact = new codepipeline.Artifact();
- 
-    const pipeline = new CdkPipeline(this, 'Pipeline', {
-      // The pipeline name
-      pipelineName: 'MyServicePipeline',
-      cloudAssemblyArtifact,
-
-      // Where the source can be found
-      sourceAction: new codepipeline_actions.GitHubSourceAction({
-        actionName: 'GitHub',
-        output: sourceArtifact,
-        oauthToken: SecretValue.secretsManager('github-token2'),
-        owner: 'maishsk',
-        repo: 'cdkpipelines-demo',
-      }),
-
-       // How it will be built and synthesized
-       synthAction: SimpleSynthAction.standardNpmSynth({
-         sourceArtifact,
-         cloudAssemblyArtifact,
-         
-         // We need a build step to compile the TypeScript Lambda
-         buildCommand: 'npm run build'
-       }),
-    });
-
-    // This is where we add the application stages
-    // ...
   }
 }
