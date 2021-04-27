2ceff59de239b44dc78e2ce2e67c0f18ddb81871
Maish Saidel-Keesing <maishsk@gmail.com>
2021-04-27 12:08:16 +0000

    changed back for demo

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   return {
-    body: 'Hello EMEA summit - how are you all???',
+    body: 'Hello from a Lambda Function',
     statusCode: 200,
   };
 }
